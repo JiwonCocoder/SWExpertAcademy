e61b709ea42d64eb0a912ed77abd1919ec76caab
unknown <iamadora@naver.com>
2019-03-07 23:35:48 +0900

    2046

---
+++ 2046.py
@@ -0,0 +1,10 @@
+'''
+caution
+1. data type which I want to use is specified in front of input
+2. if I want to change print segmentation, specify end=' '
+'''
+number = int(input())
+for i in range(0,number):
+    print('#',end='')
+
+    
